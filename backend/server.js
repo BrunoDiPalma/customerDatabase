001a5a5ac59c80d03d9a138201ab7dc96f3f5d93
BrunoDiPalma <brunocamargo2010@live.com>
2026-02-05 16:02:03 -0300

    config: backend conectado ao MySQL e servidor iniciado

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 import app from "./src/app.js"
+import pool from "./src/database/database.js"
+
+async function testarConexao() {
+  try {
+    const [rows] = await pool.query("SELECT NOW()")
+    console.log("Conectado ao MySQL ✅", rows)
+  } catch (error) {
+    console.error("Erro ao conectar ao MySQL ❌:", error)
+  }
+}
+
+testarConexao()
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-    console.log(`O servidor está rodando na porta ${PORT}`)
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
